05f97c8382940558ab66c713a211e760b722ec6e
Виктор Маковцев <svixx@mail.ru>
2024-02-27 20:55:26 +0700

    Удалены закомментированные строки

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,18 +6,12 @@ public class ArrayStorage {
     int size = 0;
 
     void clear() {
-//        if (size() > 0) {
-//            for (int i = size() - 1; i >= 0; i--) storage[i] = null;
-//        }
         while (size > 0) {
             storage[--size] = null;
         }
     }
 
     void save(Resume r) {
-//        for (Resume res : getAll()) {
-//            if (res.uuid.equals(r.uuid)) return;
-//        }
         if (size > 0) {
             for (int i = 0; i < size; i++) {
                 if (storage[i].uuid.equals(r.uuid)) return;
@@ -27,15 +21,6 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-//        if (size > 0) {
-//            Resume result = new Resume();
-//            for (Resume r : getAll()) {
-//                if (r.uuid.equals(uuid)) result = r;
-//            }
-//            return result;
-//        } else {
-//            return null;
-//        }
         if (size == 0) return null;
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) return storage[i];
@@ -47,13 +32,6 @@ public class ArrayStorage {
         if (size > 0) {
             for (int i = 0; i < size(); i++) {
                 if (storage[i].uuid.equals(uuid)) {
-//                    if (i == storage.length - 1) storage[i] = null;
-//                    else {
-//                        for (int j = i; j < size(); j++) {
-//                            if (j < storage.length - 1) storage[j] = storage[j + 1];
-//                            else storage[j] = null;
-//                        }
-//                    }
                     storage[i] = storage[--size];
                     storage[size] = null;
                 }
@@ -75,12 +53,6 @@ public class ArrayStorage {
     }
 
     int size() {
-/*
-        int i = 0;
-        while (storage[i] != null) {
-            i++;
-        }
-*/
         return size;
     }
 }
