c2a5bf0265317a84c8cb85b67054c08acdfd8b3b
Виктор Маковцев <svixx@mail.ru>
2024-03-17 22:26:28 +0700

    Метод save() в классе SortedArrayStorage

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -11,7 +11,22 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void save(Resume r) {
-
+        int index = Arrays.binarySearch(storage, 0, size, r);
+        if (size == STORAGE_LIMIT) {
+            System.out.println("Нет возможности добавить резюме. База заполнена.");
+        } else if (index >= 0) {
+            System.out.println("Резюме с uuid: " + r.getUuid() + " уже имеется в базе.");
+        } else if (-index - 1 < size) {
+            System.arraycopy(storage, -index - 1, storage, -index, size - index + 1);
+//            for (int i = size; i >= -index; i--) {
+//                storage[i] = storage[i - 1];
+//            }
+            storage[-index - 1] = r;
+            size++;
+        } else {
+            storage[-index - 1] = r;
+            size++;
+        }
     }
 
     @Override
